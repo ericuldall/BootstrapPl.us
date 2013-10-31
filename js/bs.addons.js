fee773230b1412a2ca4ba9d1700dfc8f9db9c4d9
Eric Uldall <ericuldall@gmail.com>
2013-10-30 23:00:39 -0700

    added ternary for optional collapse on closed

--- js/bs.addons.js
+++ js/bs.addons.js
@@ -40,11 +40,10 @@ var bs_addons = {
             var body = panel.find('.panel-body');
             heading.attr('data-toggle', 'collapse');
             heading.attr('data-target', '#panelBody' + bs_addons.getCollapsePanelKey());
-            $('<div id="panelBody' + bs_addons.getCollapsePanelKey() + '" class="panel-collapse"></div>').insertBefore(body);
+            $('<div id="panelBody' + bs_addons.getCollapsePanelKey() + '" class="panel-collapse'+($(this).hasClass('closed') ? ' collapse' : '')+'"></div>').insertBefore(body);
             $('#panelBody' + bs_addons.getCollapsePanelKey()).html(body.clone());
             body.remove();
             if( $(this).hasClass('closed') ){
-                $(this).collapse('toggle');
                 heading.append(collapse_down);
             }else{
                 heading.append(collapse_up);
